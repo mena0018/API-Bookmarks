da25de5193b8298b75db8f3e5f9dfa5802839750
mena0018 <rabie.menad@etudiant.univ-reims.r>
2022-12-07 15:57:18 +0100

    Création de contrôleur : GetMeController.

---
+++ src/Controller/GetMeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class GetMeController extends AbstractController
+{
+    public function __invoke(): User
+    {
+        // Récupère l'utilisateur courant depuis les données de session.
+        /** @var User $currentUser */
+        $currentUser = $this->getUser();
+
+        if (null === $currentUser) {
+            throw $this->createNotFoundException('Aucun utilisateur courant depuis les données de session');
+        }
+
+        return $currentUser;
+    }
+}
