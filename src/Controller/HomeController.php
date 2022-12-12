01fa833b098cc55841d7db1d6e65243addcb7107
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-12-12 16:12:58 +0100

    Mise en place d'un HomeController afin de rediriger sur /api.

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): RedirectResponse
+    {
+        return $this->redirect('/api');
+    }
+}
