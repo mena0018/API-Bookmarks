d6add386cd16f75313176b49669349bbdd86527f
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-12-12 13:59:55 +0100

    Modification des tests des actions PUT et PATCH de l'entité User pour vérifier la présence de la propriété mail.

--- tests/Api/User/UserPatchCest.php
+++ tests/Api/User/UserPatchCest.php
@@ -16,6 +16,7 @@ class UserPatchCest
             'login' => 'string',
             'firstname' => 'string',
             'lastname' => 'string',
+            'mail' => 'string:email',
         ];
     }
 
--- tests/Api/User/UserPutCest.php
+++ tests/Api/User/UserPutCest.php
@@ -16,6 +16,7 @@ class UserPutCest
             'login' => 'string',
             'firstname' => 'string',
             'lastname' => 'string',
+            'mail' => 'string:email',
         ];
     }
 
