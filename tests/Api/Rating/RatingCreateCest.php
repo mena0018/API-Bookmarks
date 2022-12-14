36906bdee26d53f8d60849bb733fb42cdb4de53e
mena0018 <rabie.menad@etudiant.univ-reims.r>
2022-12-14 14:12:49 +0100

    Ajout du test d'accès à l'action de création d'une note.

--- tests/Api/Rating/RatingCreateCest.php
+++ tests/Api/Rating/RatingCreateCest.php
@@ -2,6 +2,9 @@
 
 namespace App\Tests\Api\Rating;
 
+use App\Factory\UserFactory;
+use App\Tests\Support\ApiTester;
+
 class RatingCreateCest
 {
     protected static function expectedProperties(): array
@@ -12,5 +15,17 @@ class RatingCreateCest
             'bookmark' => 'string:path',
             'value' => 'integer',
         ];
+    }
+
+    public function anonymousUserCantCreateNote(ApiTester $I): void
+    {
+        // 1. 'Arrange'
+        UserFactory::createOne();
+
+        // 2. 'Act'
+        $I->sendPost('/api/ratings');
+
+        // 3. 'Assert'
+        $I->seeResponseCodeIs(401);
     }
 }
