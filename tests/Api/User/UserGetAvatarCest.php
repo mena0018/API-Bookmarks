4cf1e70cc58d5e8201bb6c504b14c6cf24c5d237
mena0018 <rabie.menad@etudiant.univ-reims.r>
2022-12-14 14:25:10 +0100

    Ajout des nouveaux scripts de test pour l'entité User

--- tests/Api/User/UserGetAvatarCest.php
+++ tests/Api/User/UserGetAvatarCest.php
@@ -2,11 +2,8 @@
 
 namespace App\Tests\Api\User;
 
-use App\Entity\User;
 use App\Factory\UserFactory;
 use App\Tests\Support\ApiTester;
-use Codeception\Attribute\DataProvider;
-use Codeception\Example;
 
 class UserGetAvatarCest
 {
@@ -22,35 +19,5 @@ class UserGetAvatarCest
         $I->seeResponseCodeIsSuccessful();
         $I->seeHttpHeader('Content-Type', 'image/png');
         $I->seeResponseContains(stream_get_contents($user->getAvatar(), -1, 0));
-    }
-
-    #[DataProvider('invalidDataLeadsToUnprocessableEntityProvider')]
-    public function invalidDataLeadsToUnprocessableEntity(ApiTester $I, Example $example): void
-    {
-        // 1. 'Arrange'
-        /** @var $user User */
-        $user = UserFactory::createOne()->object();
-        $I->amLoggedInAs($user);
-
-        // 2. 'Act'
-        $dataPut = [
-            $example['property'] => $example['value'],
-        ];
-
-        $I->sendPut('/api/users/1', $dataPut);
-
-        // 3. 'Assert'
-        $I->seeResponseCodeIs(422);
-        $I->seeResponseIsJson();
-    }
-
-    protected function invalidDataLeadsToUnprocessableEntityProvider(): array
-    {
-        return [
-            ['property' => 'login', 'value' => '<&">'],
-            ['property' => 'firstname', 'value' => '<&">'],
-            ['property' => 'lastname', 'value' => '<&">'],
-            ['property' => 'mail', 'value' => 'badmail'],
-        ];
     }
 }
