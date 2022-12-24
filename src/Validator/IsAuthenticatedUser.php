ac63d40a39fe87021aa075b2f78961517f626a50
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-12-24 08:45:03 +0100

    Ajout de la contrainte IsAuthenticatedUser.

---
+++ src/Validator/IsAuthenticatedUser.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Validator;
+
+use Symfony\Component\Validator\Constraint;
+
+#[\Attribute]
+class IsAuthenticatedUser extends Constraint
+{
+    /*
+     * Any public properties become valid options for the annotation.
+     * Then, use these in your validator class.
+     */
+    public $message = 'L\'utilisateur "{{ user }}" devrait être connecté.';
+}
